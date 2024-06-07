27f9d222e44020b92bbe71344687c3a70334fa5f
rokitgg <pintosdsgn@gmail.com>
2024-06-07 02:07:26 +0200

    feat: Add lazy loading for avatar image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { Suspense } from "react";
+
+import { Avatar } from "@/components/server/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
+
   return (
-    <main className="flex flex-1 p-16 min-h-screen items-center justify-center">
-      <h1 className="text-5xl">Based Avatar</h1>
+    <main className="flex flex-1 flex-col gap-12 p-16 min-h-screen items-center justify-center">
+      <Suspense fallback={<Skeleton className="rounded-md" style={{ width: 250, height: 250 }} />}>
+        <Avatar />
+      </Suspense>
+      <h1 className="text-5xl md:text-7xl font-semibold">Based Avatar<span className="text-blue-600">.</span></h1>
     </main>
   );
 }
